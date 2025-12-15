c3da66a0dbd4ce7a592281be04f2f8b3d529689c
ass44ma <assma.boutezrout@gmail.com>
2025-12-15 17:16:16 +0100

    Initial blog setup

--- student_script.sh
+++ student_script.sh
@@ -15,8 +15,13 @@ wc -l posts/first_post.md >> blog_stats.txt
 wc -w posts/* >> blog_stats.txt
 head -n 1 posts/second_post.md >> blog_stats.txt
 tail -n 1 config/site_config.txt >> blog_stats.txt
-cat blog_stats.txt
 
+chmod 600 config/site_config.txt
+chmod 644 posts/*.md
 
+ls -l config/* && ls -l posts/*
+
+git init
+git
 
 

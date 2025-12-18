e9b282eaff0a48868f78f39cf573dd4053e334fb
ass44ma <assma.boutezrout@gmail.com>
2025-12-18 20:55:53 +0100

    correction

--- student_script.sh
+++ student_script.sh
@@ -9,19 +9,27 @@ echo "# Learning Git" > second_post.md && echo "Today I learned about version co
 cd..&& echo "# Draft: Advanced Linux Coming soon..." > drafts/upcoming.md
 echo "Site Title: My Tech Blog Author: Your Name Theme: Dark Mode" > config/site_config.txt
 
-find posts -type f | wc -l > blog_stats.txt
+ls posts | wc -l > blog_stats.txt #total number of files in posts 
 
-wc -l posts/first_post.md >> blog_stats.txt
-wc -w posts/* >> blog_stats.txt
+wc -l posts/first_post.md >> blog_stats.txt # total number of lines in first_post.txt
+
+wc -w posts/*.md >> blog_stats.txt #total number of words in .md in the blog_stats.txt
+
 head -n 1 posts/second_post.md >> blog_stats.txt
 tail -n 1 config/site_config.txt >> blog_stats.txt
 
-chmod 600 config/site_config.txt
-chmod 644 posts/*.md
+chmod 600 config/site_config.txt # make file private
+chmod 644 posts/*.md # make file only user can edit and all users can read it
 
 ls -l config/* && ls -l posts/*
 
 git init
-git
+git config --global user.name "username"
+git config --global user.email "email"
+git add .
+git status
+git commit-m "Initial blog setup"
+git log
+git push
 
 
